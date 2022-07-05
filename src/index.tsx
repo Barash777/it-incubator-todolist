76f33895494e0f92dd0c83f7a427e179257c15a3
Barash777 <barash.vertihvost@gmail.com>
2022-07-05 03:07:01 +0300

    Heroku... add new scripts again

--- src/index.tsx
+++ src/index.tsx
@@ -15,9 +15,4 @@ serviceWorker.unregister();
 // "start": "node index.js",
 
 /*
-"scripts": {
-    "start": "react-scripts start",
-        "build": "react-scripts build",
-        "test": "react-scripts test",
-        "eject": "react-scripts eject"
-},*/
+*/
